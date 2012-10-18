d793639a25d5e0c2b7f6266fce62be395060ca9a
cmb69 <cmbecker69@gmx.de>
2012-10-18 20:25:54 +0000

    added Dutch translation (contributed by roze)

---
+++ languages/nl.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Rob Zeijen
+ *
+ * This work is licensed under a Creative Commons Attribution 3.0 Unported
+ * License.
+ */
+
+$plugin_tx['privacy']['message']="<p>We willen graag cookies plaatsen op uw computer zodat onze website beter werkt. Wilt u meer weten over cookies, kijk dan op <a href=\"?Privacy_notice\">privacy bepalingen</a>.</p>";
+$plugin_tx['privacy']['checkbox']="Ik accepteer cookies van deze website.";
+$plugin_tx['privacy']['button']="Ga verder";
+$plugin_tx['privacy']['syscheck_title']="Systeem controle";
+$plugin_tx['privacy']['syscheck_phpversion']="PHP versie &ge; %s";
+$plugin_tx['privacy']['syscheck_extension']="Extensie '%s' is geladen";
+$plugin_tx['privacy']['syscheck_encoding']="geconfigureerd voor 'UTF-8' codering.";
+$plugin_tx['privacy']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['privacy']['syscheck_writable']="Map '%s' beschrijfbaar";
+$plugin_tx['privacy']['cf_duration']="Het aantal dagen dat de overeenkomst geldig is. \"0\" betekent alleen voor deze sessie, d.w.z. totdat de browser afgesloten wordt.";
+
+?>
