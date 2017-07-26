7355641a07c492ff24ddf21beebde84d9f8dbf55
Christoph M. Becker <cmbecker69@gmx.de>
2017-07-26 18:19:00 +0200

    Add German localization

---
+++ languages/de.php
@@ -0,0 +1,21 @@
+<?php
+
+$plugin_tx['privacy']['message']="<p>Wir würden gerne Cookies auf Ihrem Rechner platzieren, um uns zu helfen diese Website zu verbessern. Erfahren sie mehr über diese Cookies in der <a href=\"?Datenschutzerklaerung\">Datenschutzerklärung</a>.</p>";
+$plugin_tx['privacy']['checkbox']="Ich akzeptiere Cookies von dieser Site.";
+$plugin_tx['privacy']['button']="Weiter";
+$plugin_tx['privacy']['info']="Info";
+
+$plugin_tx['privacy']['alt_logo']="Schloss";
+
+$plugin_tx['privacy']['syscheck_fail']="Fehler";
+$plugin_tx['privacy']['syscheck_message']="Prüfe, dass %1\$s … %2\$s";
+$plugin_tx['privacy']['syscheck_phpversion']="die PHP-Version ≥ %s";
+$plugin_tx['privacy']['syscheck_success']="OK";
+$plugin_tx['privacy']['syscheck_title']="System-Prüfung";
+$plugin_tx['privacy']['syscheck_warning']="Warnung";
+$plugin_tx['privacy']['syscheck_writable']="'%s' schreibbar ist";
+$plugin_tx['privacy']['syscheck_xhversion']="die CMSimple_XH-Version ≥ %s";
+
+$plugin_tx['privacy']['cf_duration']="Die Anzahl von Tagen, die die Zustimmung gültig ist. \"0\" bedeutet nur für die Sitzung, d.h. bis der Browser geschlossen wird.";
+
+?>
