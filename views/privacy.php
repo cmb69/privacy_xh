bf72ed38f35c4f09da1911d045d0830f65dd2cb3
Christoph M. Becker <cmbecker69@gmx.de>
2019-02-05 23:22:40 +0100

    Output HTML5

--- views/privacy.php
+++ views/privacy.php
@@ -5,8 +5,8 @@
         <button type="submit" class="submit" name="privacy_agree" value="yes"><?=$this->text('label_allow')?></button>
     </p>
 </form>
-<script type="text/javascript">/* <![CDATA[ */
+<script>
     if (typeof navigator.cookieEnabled != "undefined" && !navigator.cookieEnabled) {
         document.forms.privacy.style.display = "none";
     }
-/* ]]> */</script>
+</script>
