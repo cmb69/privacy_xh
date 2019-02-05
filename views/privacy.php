17ceb5f8daa29aea90daf4497c9a9e6b8bd283b1
Christoph M. Becker <cmbecker69@gmx.de>
2019-02-05 23:22:12 +0100

    Move form buttons to the right

--- views/privacy.php
+++ views/privacy.php
@@ -1,8 +1,8 @@
 <form id="privacy" name="privacy" method="post">
     <?=$this->message()?>
-    <p>
-        <button type="submit" class="submit" name="privacy_agree" value="yes"><?=$this->text('label_allow')?></button>
+    <p class="privacy_buttons">
         <button type="submit" class="submit" name="privacy_agree" value="no"><?=$this->text('label_decline')?></button>
+        <button type="submit" class="submit" name="privacy_agree" value="yes"><?=$this->text('label_allow')?></button>
     </p>
 </form>
 <script type="text/javascript">/* <![CDATA[ */
