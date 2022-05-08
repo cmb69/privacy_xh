a67bcc6734481fedfb0c140c4f1fd1cb1422cb31
Christoph M. Becker <cmbecker69@gmx.de>
2022-05-08 17:17:32 +0200

    Remove mostly useless copyright info from plugin info screen

--- views/info.php
+++ views/info.php
@@ -6,41 +6,14 @@ use stdClass;
 
 /**
  * @var View $this
- * @var string $logo
  * @var string $version
  * @var array<stdClass> $checks
  */
 
 ?>
 
-<h1>Privacy – <?=$this->text('info')?></h1>
-<img class="privacy_logo" src="<?=$this->escape($logo)?>" alt="<?=$this->text('alt_logo')?>" />
-<p>
-    Version: <?=$this->escape($version)?>
-</p>
-<p>
-    Copyright © Christoph M. Becker
-</p>
-<p class="privacy_license">
-    Privacy_XH is free software: you can redistribute it and/or modify it under
-    the terms of the GNU General Public License as published by the Free
-    Software Foundation, either version 3 of the License, or (at your option)
-    any later version.
-</p>
-<p class="privacy_license">
-    Privacy_XH is distributed in the hope that it will be useful, but
-    <em>without any warranty</em>; without even the implied warranty of
-    <em>merchantability</em> or <em>fitness for a particular purpose</em>. See
-    the GNU General Public License for more details.
-</p>
-<p class="privacy_license">
-    You should have received a copy of the GNU General Public License along with
-    Privacy_XH. If not, see <a target="_blank"
-    href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
-</p>
-<div class="privacy_syscheck">
-    <h2><?=$this->text('syscheck_title')?></h2>
+<h1>Privacy <?=$this->escape($version)?></h1>
+<h2><?=$this->text('syscheck_title')?></h2>
 <?php foreach ($checks as $check):?>
-    <p class="xh_<?=$this->escape($check->state)?>"><?=$this->text('syscheck_message', $check->label, $check->stateLabel)?></p>
+<p class="xh_<?=$this->escape($check->state)?>"><?=$this->text('syscheck_message', $check->label, $check->stateLabel)?></p>
 <?php endforeach?>
-</div>
