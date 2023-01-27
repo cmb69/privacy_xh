08ba8c28bc03cdbfbd6b547c92130436b63a3575
Christoph M. Becker <cmbecker69@gmx.de>
2023-01-27 23:44:24 +0100

    Actually remove Plugin

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,6 @@ const PRIVACY_VERSION = '1.0beta3';
 require_once "../../cmsimple/functions.php";
 
 require_once "./classes/HtmlString.php";
-require_once "./classes/Plugin.php";
 require_once "./classes/ShowInfo.php";
 require_once "./classes/SystemChecker.php";
 require_once "./classes/View.php";
