712ad26819a4dae5f4fa64cf1c8bcbc67ef38b4f
Christoph M. Becker <cmbecker69@gmx.de>
2023-01-27 22:34:12 +0100

    Indent XML files and view templates with two spaces

--- views/privacy.php
+++ views/privacy.php
@@ -10,14 +10,14 @@ namespace Privacy;
 ?>
 
 <form id="privacy" name="privacy" method="post">
-    <?=$this->escape($message)?>
-    <p class="privacy_buttons">
-        <button type="submit" class="submit" name="privacy_agree" value="no"><?=$this->text('label_decline')?></button>
-        <button type="submit" class="submit" name="privacy_agree" value="yes"><?=$this->text('label_allow')?></button>
-    </p>
+  <?=$this->escape($message)?>
+  <p class="privacy_buttons">
+    <button type="submit" class="submit" name="privacy_agree" value="no"><?=$this->text('label_decline')?></button>
+    <button type="submit" class="submit" name="privacy_agree" value="yes"><?=$this->text('label_allow')?></button>
+  </p>
 </form>
 <script>
-    if (typeof navigator.cookieEnabled != "undefined" && !navigator.cookieEnabled) {
-        document.forms.privacy.style.display = "none";
-    }
+  if (typeof navigator.cookieEnabled != "undefined" && !navigator.cookieEnabled) {
+    document.forms.privacy.style.display = "none";
+  }
 </script>
