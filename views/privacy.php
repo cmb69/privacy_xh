770621a35713b54aa42f55fe600fd56023f34eb1
Christoph M. Becker <cmbecker69@gmx.de>
2023-03-13 17:23:33 +0100

    Close #13 (Improve styling)
    We go with a hopefully sensible middle-ground between the simplistic
    design we had, and the elaborate designs that can be seen on many
    Websites.  Webmasters and -designers can adapt to their needs.

--- views/privacy.php
+++ views/privacy.php
@@ -10,17 +10,19 @@ if (!defined("CMSIMPLE_XH_VERSION")) {header("HTTP/1.1 403 Forbidden"); exit;}
  */
 ?>
 <!-- privacy form -->
-<form id="privacy" name="privacy" method="post">
+<div id="privacy">
+  <form name="privacy" method="post">
 <?if (isset($message)):?>
-  <div><?=$message?></div>
+    <div><?=$message?></div>
 <?else:?>
-  <p><?=$this->text('message')?></p>
+    <p><?=$this->text('message')?></p>
 <?endif?>
-  <p class="privacy_buttons">
-    <button type="submit" class="submit" name="privacy_agree" value="no"><?=$this->text('label_decline')?></button>
-    <button type="submit" class="submit" name="privacy_agree" value="yes"><?=$this->text('label_allow')?></button>
-  </p>
-</form>
+    <p class="privacy_buttons">
+      <button type="submit" class="submit" name="privacy_agree" value="no"><?=$this->text('label_decline')?></button>
+      <button type="submit" class="submit" name="privacy_agree" value="yes"><?=$this->text('label_allow')?></button>
+    </p>
+  </form>
+</div>
 <script>
 if (typeof navigator.cookieEnabled != "undefined" && !navigator.cookieEnabled) {
     document.forms.privacy.style.display = "none";
